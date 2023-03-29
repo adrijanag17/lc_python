31b1610236189805aed30259050ad503495f635e
Adrija <71489040+adrijanag17@users.noreply.github.com>
2023-03-28 20:10:09 -0700

    lc 371 solution

--- sum_two_integers.py
+++ sum_two_integers.py
@@ -1,18 +1,28 @@
 # time - O(1) space - O(1)
 
 def getSum(a: int, b: int)-> int:
+
+    # mask needed in python to limit size of integer to 32 bit
+    mask = 0xffffffff
+
     while (b != 0):
         c = (a & b) << 1
-        a = a ^ b
-        b = c
+        a = (a ^ b) & mask
+        b = c & mask
 
-    return a
+    # if a is negative
+    if a > mask // 2:
+        return ~(a ^ mask)
+    else:
+        return a
 
 
 def main():
 
     print(getSum(2, 3))
     print(getSum(7, 7))
+    print(getSum(-1, -3))
+    print(getSum(-1, 3))
 
 
 main()
