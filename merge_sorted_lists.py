5bb72eeea582645ded2dd7714bcf21e47154cf83
Adrija <71489040+adrijanag17@users.noreply.github.com>
2023-05-28 20:30:10 -0700

    lc 21 solution

---
+++ merge_sorted_lists.py
@@ -0,0 +1,30 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        this.val = val
+        this.next = next
+
+
+class Solution:
+    
+    # time - O(n) space - O(1)
+    def merge_two_lists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+
+        else:
+            tail.next = list2
+
+        return dummy.next
