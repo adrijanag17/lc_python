68ac7f3bd3040a1d175ec3996487206f52e26733
Adrija <71489040+adrijanag17@users.noreply.github.com>
2023-06-25 14:10:56 -0700

    lc 347 alternate solution

--- top_k_freq_elements.py
+++ top_k_freq_elements.py
@@ -24,9 +24,30 @@ def top_k_freq(nums: list[int], k: int) -> list[int]:
     return res
 
 
+# time - O(n) space - O(n)
+def top_k_bucket(nums: list[int], k: int) -> list[int]:
+    freq = {}
+
+    for n in nums:
+        freq[n] = freq.get(n, 0) + 1
+
+    buckets = [[] for _ in range(len(nums) + 1)]
+
+    for key, val in freq.items():
+        buckets[val].append(key)
+
+    res = []
+
+    for i in range(len(buckets) - 1, 0, -1):
+        for n in buckets[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
+
+
 def main():
-    print(top_k_freq([1, 1, 1, 2, 2, 3], 2))
-    print(top_k_freq([1], 1))
+    print(top_k_bucket([1, 1, 1, 2, 2, 3], 2))
+    print(top_k_bucket([1], 1))
 
 
 main()
