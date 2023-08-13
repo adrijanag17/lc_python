1ff37d04dad3f6eda6f8d459a8c42f3b1bb45b44
Adrija <71489040+adrijanag17@users.noreply.github.com>
2023-08-12 20:32:04 -0700

    lc 1448

---
+++ count_good_nodes.py
@@ -0,0 +1,28 @@
+class TreeNode:
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    # time - O(n) space - O(n)
+    def goodNodes(self, root: TreeNode) -> int:
+
+        count = 0
+        def dfs(node, max):
+            nonlocal count
+            if not node:
+                return
+            
+            if node.val >= max:
+                count += 1
+                max = node.val
+
+            dfs(node.left, max)
+            dfs(node.right, max)
+
+        dfs(root, root.val)
+        return count
